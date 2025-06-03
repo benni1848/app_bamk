c8adc6ea429bbd467a00eed3947287c857142a29
Locha <164514709+0001-1100-1001-1011@users.noreply.github.com>
2025-06-03 16:00:31 +0200

    AppBar structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_bamk/presentation/login_page/login_page.dart';
 import 'package:app_bamk/presentation/registration_page/registration_page.dart';
+import 'package:app_bamk/presentation/search_page/search_page.dart';
 import 'package:app_bamk/presentation/ticketView_page/ticketView_page.dart';
 import 'package:app_bamk/presentation/userProfile_page/userProfile_page.dart';
 import 'package:flutter/material.dart';
@@ -16,12 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "BAMK Rating",
-      initialRoute: "/", //Anpassen, um direkt in die Ansicht zu starten
+      initialRoute: "/search", //Anpassen, um direkt in die Ansicht zu starten
       routes: {
         "/": (context) => const LoginPage(),
         "/registration": (context) => const RegistrationPage(),
         "/ticket": (context) => const TicketView(),
         "/user": (context) => const UserProfile(),
+        "/search":(context) => const SearchPage(),
       },
     );
   }
--- lib/presentation/search_page/search_page.dart
+++ lib/presentation/search_page/search_page.dart
@@ -1,4 +1,4 @@
-import 'package:app_bamk/presentation/search_page/widgets/search_page_form.dart';
+//import 'package:app_bamk/presentation/search_page/widgets/search_page_form.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
@@ -6,7 +6,35 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-          backgroundColor: Color(0xFF1a1a1a), body: SearchPageForm());
+    return  Scaffold(
+      backgroundColor: const Color(0xFF1a1a1a),     // Alternativ: Colors.grey[900]
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF80B5E9), // Background for AppBar
+          automaticallyImplyLeading: false,     // Remove "Back-Button" from AppBar
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/test.png', 
+                height: 40,
+              ),
+              
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'Inhalte durchsuchen',
+                  ),
+                ), 
+              ),
+            ], // End of child
+          ),
+        ),
+
+        body:const Center(
+          child: Text(
+            'Inhalt folgt...'
+          ),
+        ), 
+      );
   }
 }
